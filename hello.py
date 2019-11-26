6e77825632473b31d02b36a436be092194f2467d
Morad Ait Abdellah <1935031@champlaincollege.qc.ca>
2019-11-26 14:16:57 -0500

    myFirstCommit

---
+++ hello.py
@@ -0,0 +1,42 @@
+# with open("d:\wu.txt","r") as file1:
+    # file_stuff=file1.read()
+    # #print (file_stuff)
+# print(file1.closed)
+# print(file_stuff)
+ 
+# a= 10
+# a*=2
+# print (a)
+# b=15
+# b=a+b
+# print (b)
+
+# a=650
+# if a%7==0:
+    # print("Gotcha")
+# else:print("Nope")
+# print("---------")
+
+# if a%7!=0:
+    # print("Nope")
+# else:print("Gotcha")
+
+# i=0
+# while i < 37:
+    # print('i')
+    # i+=1
+myage=50
+redge=myage+2
+ledge=2
+print('         ')
+print('         ')
+print('          '+'i'*myage)
+print('        |'+'‾'*redge+'|')
+print('        |'+' '*redge+'|')
+print('        |'+' '*redge+'|')
+print('        |'+'_'*redge+'|')
+
+
+
+    
+    
